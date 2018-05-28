f2c458759732ba6bd884aa25f358723b66c3d5a8
rinsuki <428rinsuki+git@gmail.com>
2018-05-28 21:50:32 +0900

    minor fix

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@
  * https://github.com/facebook/react-native
  */
 
-import React, { Component } from 'react';
+import React = require('react')
 import {
     Platform,
     StyleSheet,
@@ -19,7 +19,7 @@ const instructions = Platform.select({
 });
 
 type Props = {};
-export default class App extends Component<Props> {
+export default class App extends React.Component<Props> {
     render() {
         return (
             <View style={styles.container}>
