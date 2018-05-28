5d0ce6d29a64a6dfed16abb2c2a66d08c4862381
rinsuki <428rinsuki+git@gmail.com>
2018-05-29 06:08:51 +0900

    footerそれっぽく

--- src/App.tsx
+++ src/App.tsx
@@ -8,8 +8,11 @@ import {
     Platform,
     StyleSheet,
     Text,
-    View
+    View,
+    AsyncStorage,
+    StatusBar,
 } from 'react-native';
+import Icon from "react-native-vector-icons/FontAwesome"
 
 const instructions = Platform.select({
     ios: 'Press Cmd+R to reload,\n' +
@@ -20,18 +23,37 @@ const instructions = Platform.select({
 
 type Props = {};
 export default class App extends React.Component<Props> {
+
+    componentDidMount() {
+    }
+
     render() {
+        const iconColor="rgba(255, 255, 255, 0.9)"
+
         return (
             <View style={styles.container}>
-                <Text style={styles.welcome}>
-                    Welcome to React Native!
-                </Text>
-                <Text style={styles.instructions}>
-                    To get started, edit App.js
-                </Text>
-                <Text style={styles.instructions}>
-                    {instructions}
-                </Text>
+                <View style={styles.main}>
+                    <Text>mainView</Text>
+                </View>
+                <View style={styles.bottomBar}>
+                    <View style={styles.bottomBarIconContainer}>
+                        <Icon name="bars" size={24} color={iconColor}/>
+                    </View>
+                    <View style={{
+                        flex: 1,
+                        borderLeftColor: "rgba(0, 0, 0, 0.1)",
+                        borderRightColor: "rgba(0, 0, 0, 0.1)",
+                        borderLeftWidth: 1,
+                        borderRightWidth: 1,
+                        justifyContent: "center",
+                    }}><Text style={{
+                        color: iconColor,
+                        textAlign: "center",
+                    }}>title</Text></View>
+                    <View style={styles.bottomBarIconContainer}>
+                        <Icon name="pencil" size={24} color={iconColor}/>
+                    </View>
+                </View>
             </View>
         );
     }
@@ -40,18 +62,19 @@ export default class App extends React.Component<Props> {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: '#F5FCFF',
     },
-    welcome: {
-        fontSize: 20,
-        textAlign: 'center',
-        margin: 10,
+    main: {
+        backgroundColor: "#fafafa",
+        flex: 1,
     },
-    instructions: {
-        textAlign: 'center',
-        color: '#333333',
-        marginBottom: 5,
+    bottomBar: {
+        flexDirection: "row",
+        backgroundColor: "#595f6f",
+        height: 48,
     },
+    bottomBarIconContainer: {
+        width: 48,
+        justifyContent: "center",
+        alignItems: "center",
+    }
 });
